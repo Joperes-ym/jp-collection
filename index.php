776bf6bb41b4f2b92ab294e4b21c0b2e4c8b8f63
Joperes-ym <joanapereira.ym@gmail.com>
2023-02-06 16:03:38 +0000

    files created

--- index.php
+++ index.php
@@ -5,6 +5,26 @@ require_once 'functions.php';
 $db = createDbConn();
 $cards = getAllCards($db);
 
-echo '<pre>';
-var_dump($cards);
-echo '<pre>';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Collection</title>
+    <link rel="stylesheet" type="text/css" href="normalize.css" />
+    <link rel="stylesheet" type="text/css" href="styles.css.css" />
+</head>
+<body>
+    <navbar>
+        <a href="#"><img src="logo.png"></a>
+        <div>
+            <ul>
+                <li><button>MY COLLECTION</button></li>
+                <li><button>ADD TO COLLECTION</button></li>
+            </ul>
+        </div>
+    </navbar>
+</body>
+</html>
